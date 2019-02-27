81152942b47814cbd402a2589d40d76eb78424c8
Pawel Halicki <phali@softserveinc.com>
2019-02-27 15:21:56 +0100

    Friends Add Friends Model, Add configuration to Friends table, Add FriendshipTypes enum

---
+++ SmartSchedule.Persistence/Configurations/FriendsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartSchedule.Persistence.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using SmartSchedule.Domain.Entities;
+
+    public class FriendsConfiguration : IEntityTypeConfiguration<Friends>
+    {
+        public void Configure(EntityTypeBuilder<Friends> builder)
+        {
+            builder.Property(x => x.Type).HasConversion<int>();
+            builder.HasKey("FirstUserId","SecoundUserId");
+        }
+    }
+}
--- SmartSchedule.Persistence/SmartScheduleDbContext.cs
+++ SmartSchedule.Persistence/SmartScheduleDbContext.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
-using SmartSchedule.Domain.Entities;
-
-namespace SmartSchedule.Persistence
+﻿namespace SmartSchedule.Persistence
 {
+    using Microsoft.EntityFrameworkCore;
+    using SmartSchedule.Domain.Entities;
+
     public class SmartScheduleDbContext : DbContext
     {
         public SmartScheduleDbContext(DbContextOptions<SmartScheduleDbContext> options) : base(options)
@@ -15,9 +12,10 @@ namespace SmartSchedule.Persistence
         public virtual DbSet<Event> Events { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Calendar> Calendars { get; set; }
-        public virtual DbSet<Location> Locations { get; set; }      
+        public virtual DbSet<Location> Locations { get; set; }
         public virtual DbSet<UserCalendar> UserCalendars { get; set; }
         public virtual DbSet<UserEvents> UserEvents { get; set; }
+        public virtual DbSet<Friends> Friends { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
