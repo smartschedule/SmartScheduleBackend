1a18b85e7dd597bf39a04b9b506e9cb679be2e93
Unknown <vaporbae@gmail.com>
2019-02-27 23:19:59 +0100

    Calendar Add controllers for UpdateCalendar, DeleteFriendFromCalendar, AddFriendToCalendar

--- SmartSchedule.Api/Controllers/CalendarController.cs
+++ SmartSchedule.Api/Controllers/CalendarController.cs
@@ -2,8 +2,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchedule.Application.Calendar.Commands.AddFriendToCalendar;
 using SmartSchedule.Application.Calendar.Commands.CreateCalendar;
 using SmartSchedule.Application.Calendar.Commands.DeleteCalendar;
+using SmartSchedule.Application.Calendar.Commands.DeleteFriendFromCalendar;
+using SmartSchedule.Application.Calendar.Commands.UpdateCalendar;
 
 namespace SmartSchedule.Api.Controllers
 {
@@ -17,6 +20,24 @@ namespace SmartSchedule.Api.Controllers
 
         [HttpPost("/api/DeleteCalendar")]
         public async Task<IActionResult> DeleteCalendar([FromBody]DeleteCalendarCommand calendar)
+        {
+            return Ok(await Mediator.Send(calendar));
+        }
+
+        [HttpPost("/api/UpdateCalendar")]
+        public async Task<IActionResult> UpdateCalendar([FromBody]UpdateCalendarCommand calendar)
+        {
+            return Ok(await Mediator.Send(calendar));
+        }
+
+        [HttpPost("/api/AddFriendToCalendar")]
+        public async Task<IActionResult> AddFriendToCalendar([FromBody]AddFriendToCalendarCommand calendar)
+        {
+            return Ok(await Mediator.Send(calendar));
+        }
+
+        [HttpPost("/api/DeleteFriendFromCalendar")]
+        public async Task<IActionResult> DeleteFriendFromCalendar([FromBody]DeleteFriendFromCalendarCommand calendar)
         {
             return Ok(await Mediator.Send(calendar));
         }
