80669782b35b33bcac882b4d4f798af712b9fcb8
Unknown <vaporbae@gmail.com>
2019-03-03 17:16:42 +0100

    Event Add Update Event
    Added command, validator, controller and tests

--- SmartSchedule.Api/Controllers/EventController.cs
+++ SmartSchedule.Api/Controllers/EventController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SmartSchedule.Application.Event.Commands.CreateEvent;
+using SmartSchedule.Application.Event.Commands.UpdateEvent;
 
 namespace SmartSchedule.Api.Controllers
 {
@@ -10,6 +11,12 @@ namespace SmartSchedule.Api.Controllers
     {
         [HttpPost("/api/CreateEvent")]
         public async Task<IActionResult> CreateEvent([FromBody]CreateEventCommand eventCommand)
+        {
+            return Ok(await Mediator.Send(eventCommand));
+        }
+
+        [HttpPost("/api/UpdateEvent")]
+        public async Task<IActionResult> UpdateEvent([FromBody]UpdateEventCommand eventCommand)
         {
             return Ok(await Mediator.Send(eventCommand));
         }
---
+++ SmartSchedule.Application/Event/Commands/UpdateEvent/UpdateEventCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartSchedule.Application.Event.Commands.UpdateEvent
+{
+    using FluentValidation;
+    using SmartSchedule.Persistence;
+    using Microsoft.EntityFrameworkCore;
+
+    public class UpdateEventCommandValidator : AbstractValidator<UpdateEventCommand>
+    {
+        public UpdateEventCommandValidator(SmartScheduleDbContext context)
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(x => x.StartDate).NotEmpty().WithMessage("You must set a start date");
+            RuleFor(x => x.EndTime).NotEmpty().WithMessage("You must set an end date");
+            RuleFor(x => x.ReminderAt).NotEmpty().WithMessage("You must set a reminder");
+            RuleFor(x => x.Latitude).NotEmpty().WithMessage("You must declare a latitude");
+            RuleFor(x => x.Longitude).NotEmpty().WithMessage("You must declare a longitude");
+            RuleFor(x => x.RepeatsEvery).NotEmpty().WithMessage("You must declare how often event repeats");
+        }
+    }
+}
