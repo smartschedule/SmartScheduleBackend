fffa6514298da3ad577e3958d079e9929db874ef
Adam Bajguz <adam.bajguz@gmail.com>
2019-04-03 12:11:28 +0200

    Experimental SonarCloud branch selection

--- SonarCloudAnalysis.bat
+++ SonarCloudAnalysis.bat
@@ -1,9 +1,15 @@
+@ECHO OFF
+ECHO SonarScaner will perform analysis on branch:
+SET BRANCH= | git symbolic-ref --short HEAD
+PAUSE
+
+@ECHO ON
 dotnet tool install --global dotnet-sonarscanner
 
-dotnet sonarscanner begin /k:"adambajguz_SmartScheduleBackend" /o:"adambajguz-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="706c2a89d52059f9c0c70ffb2741e9263bcf39a4" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.cs.opencover.reportsPaths="%cd%\lcov.opencover.xml"
+dotnet sonarscanner begin /k:"adambajguz_SmartScheduleBackend" /o:"adambajguz-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="706c2a89d52059f9c0c70ffb2741e9263bcf39a4" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.cs.opencover.reportsPaths="%cd%\lcov.opencover.xml" /d:sonar.branch.name="develop" /d:sonar.branch.target="develop"
 dotnet restore
 dotnet build
 dotnet test ./SmartSchedule.Test/SmartSchedule.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
 dotnet sonarscanner end /d:sonar.login="706c2a89d52059f9c0c70ffb2741e9263bcf39a4"
 
-PAUSE
+@PAUSE
