1a1e86b175bd53127523fcf8274f0e0e159c71f6
Adam Bajguz <adam.bajguz@gmail.com>
2019-05-06 20:54:07 +0200

    Renamed file

---
+++ SmartSchedule.Application.DTO/Friends/Queries/FriendsListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartSchedule.Application.DTO.Friends.Queries
+{
+    using System.Collections.Generic;
+    using SmartSchedule.Application.DTO.User;
+
+    public class FriendsListResponse
+    {
+        public List<UserLookupModel> Users { get; set; }
+    }
+}
