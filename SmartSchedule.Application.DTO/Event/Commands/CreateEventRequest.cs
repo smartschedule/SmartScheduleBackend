b46aa8f80e516d99eb2cb740c960435264dfc387
Adam Bajguz <adam.bajguz@gmail.com>
2019-05-06 20:49:58 +0200

    Renamed two files

---
+++ SmartSchedule.Application.DTO/Event/Commands/CreateEventRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace SmartSchedule.Application.DTO.Event.Commands
+{
+    using System;
+    using SmartSchedule.Domain.Enums;
+
+    public class CreateEventRequest
+    {
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public TimeSpan? ReminderBefore { get; set; }
+
+        public TimeSpan? RepeatsEvery { get; set; }
+        public DateTime? RepeatsTo { get; set; }
+
+        public EventTypes Type { get; set; }
+
+        public string Name { get; set; }
+        public string ColorHex { get; set; }
+
+        public int CalendarId { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+}
