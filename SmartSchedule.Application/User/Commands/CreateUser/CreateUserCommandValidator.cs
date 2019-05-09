006d036e741d8ded88b709d82a9651911f265ad0
Adam Bajguz <adam.bajguz@gmail.com>
2019-05-09 11:38:39 +0200

    User extra validation

--- SmartSchedule.Application/User/Commands/CreateUser/CreateUserCommandValidator.cs
+++ SmartSchedule.Application/User/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -8,10 +8,12 @@
     public class CreateUserCommandValidator : AbstractValidator<CreateUserRequest>
     {
         private const int MIN_PASSWORD_LENGTH = 8;
+        private const int MIN_USERNAME_LENGTH = 3;
 
         public CreateUserCommandValidator(SmartScheduleDbContext context)
         {
             RuleFor(x => x.UserName).NotEmpty().WithMessage("You must set username");
+            RuleFor(x => x.UserName).MinimumLength(MIN_PASSWORD_LENGTH).WithMessage("Username must have 3 or more characters");
             RuleFor(x => x.Email).EmailAddress().NotEmpty().MustAsync(async (request, val, token) =>
             {
                 var userResult = await context.Users.FirstOrDefaultAsync(x => x.Email.Equals(val));
@@ -23,7 +25,8 @@
 
                 return false;
             }).WithMessage("This email is already in use.");
-            RuleFor(x => x.Password).MinimumLength(MIN_PASSWORD_LENGTH).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty().WithMessage("You must set password"); ;
+            RuleFor(x => x.Password).MinimumLength(MIN_PASSWORD_LENGTH).WithMessage("Password must have 3 or more characters");
         }
     }
 }
