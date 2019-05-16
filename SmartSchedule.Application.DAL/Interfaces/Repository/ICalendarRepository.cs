b926ccabcb6946aa11982bc94b8a378d878cc3d9
Adam Bajguz <adam.bajguz@gmail.com>
2019-05-16 09:25:54 +0200

    Sepcific repo

--- SmartSchedule.Application.DAL/Interfaces/Repository/ICalendarRepository.cs
+++ SmartSchedule.Application.DAL/Interfaces/Repository/ICalendarRepository.cs
@@ -1,10 +1,11 @@
 ﻿namespace SmartSchedule.Application.DAL.Interfaces.Repository
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using SmartSchedule.Application.DAL.Interfaces.Repository.Generic;
+    using SmartSchedule.Domain.Entities.Base;
 
-    public interface ICalendarRepository
+    public interface ICalendarRepository<TEntity, TId> : IGenericRepository<TEntity, TId>
+        where TEntity : class, IBaseEntity<TId> where TId : IComparable
     {
     }
 }
--- SmartSchedule.Infrastucture/Repository/Generic/GenericRepository.cs
+++ SmartSchedule.Infrastucture/Repository/Generic/GenericRepository.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
-    using SmartSchedule.Application.DAL.Interfaces.Repository;
+    using SmartSchedule.Application.DAL.Interfaces.Repository.Generic;
     using SmartSchedule.Domain.Entities.Base;
 
     public class GenericRepository<TEntity, TId> : GenericReadOnlyRepository<TEntity, TId>, IGenericRepository<TEntity, TId>
