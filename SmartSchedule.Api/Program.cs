fb1ad9a8397318db8718314315aff1a230c6e5cd
Adam Bajguz <adam.bajguz@gmail.com>
2019-05-30 13:46:57 +0200

    Serilog

--- SmartSchedule.Api/Program.cs
+++ SmartSchedule.Api/Program.cs
@@ -1,18 +1,49 @@
 ﻿namespace SmartSchedule.Api
 {
+    using System;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Serilog;
+    using Serilog.Events;
 
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                              .MinimumLevel.Debug()
+                              .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                              .Enrich.FromLogContext()
+                              .WriteTo.Async(a => a.Console())
+                              .WriteTo.Async(a => a.File("logs/myapp.log",
+                                                         fileSizeLimitBytes: 1_000_000,
+                                                         rollingInterval: RollingInterval.Day,
+                                                         rollOnFileSizeLimit: true,
+                                                         shared: true))
+                              .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                    .UseStartup<Startup>()
+                   .UseSerilog()
                    .UseUrls("http://localhost:2137", "http://localhost:2138");
+
     }
 }
