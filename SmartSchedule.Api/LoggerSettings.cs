7b9b018b6b676d4a4a31e419cd56563a0fe4c71f
Adam Bajguz <adam.bajguz@gmail.com>
2019-06-10 22:41:35 +0200

    Logger config file

---
+++ SmartSchedule.Api/LoggerSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartSchedule.Api
+{
+    public class LoggerSettings
+    {
+        public string FilePath { get; set; }
+        public string FileNameTemplate { get; set; }
+        public string FullPath { get => FilePath + FileNameTemplate; }
+
+        public int FileSizeLimitInBytes { get; set; }
+        public int FlushIntervalInSeconds { get; set; }
+
+        public string FileOutputTemplate { get; set; }
+
+        public string ConsoleOutputTemplate { get; set; }
+    }
+}
