48fe0b2152a18b0d74a5dd3be724b6eb08e29b41
Adam Bajguz <adam.bajguz@gmail.com>
2019-06-10 21:40:39 +0200

    Created interface

---
+++ SmartSchedule.Application/Interfaces/ISmartScheduleDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SmartSchedule.Application.Interfaces
+{
+    using Microsoft.EntityFrameworkCore;
+    using SmartSchedule.Domain.Entities;
+
+    public interface ISmartScheduleDbContext
+    {
+        DbSet<Calendar> Calendars { get; set; }
+        DbSet<Event> Events { get; set; }
+        DbSet<Friends> Friends { get; set; }
+        DbSet<Location> Locations { get; set; }
+        DbSet<UserCalendar> UserCalendars { get; set; }
+        DbSet<UserEvent> UserEvents { get; set; }
+        DbSet<User> Users { get; set; }
+    }
+}
